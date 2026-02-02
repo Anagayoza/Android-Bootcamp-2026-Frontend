c6b8a729f9d0ddabbd3e062141ffe7d6b7536a51
Anagayoza <girfanov2007@gmail.com>
2026-02-02 17:56:33 +0300

    core: добавлено REST-взаимодействие
    Выполнено задание 5, наверное.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "ru.sicampus.bootcamp2026"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "ru.sicampus.bootcamp2026"
         minSdk = 25
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
@@ -44,11 +44,19 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+
+    implementation(libs.coil.compose)
+//    implementation(libs.coil.network.ktor3)
+    implementation(libs.bundles.ktor)
+    implementation(libs.kotlinx.serialization.json)
+    implementation("io.coil-kt.coil3:coil-network-ktor3:3.3.0")
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
---
+++ app/src/main/java/ru/sicampus/bootcamp2026/ui/screen/users/UsersState.kt
@@ -0,0 +1,11 @@
+package ru.sicampus.bootcamp2026.ui.screen.users
+
+import ru.sicampus.bootcamp2026.domain.entities.UserEntity
+
+sealed interface UsersState {
+    data class Error(val reason: String): UsersState
+    data object Loading: UsersState
+    data class Content(
+        val users: List<UserEntity>
+    ): UsersState
+}
